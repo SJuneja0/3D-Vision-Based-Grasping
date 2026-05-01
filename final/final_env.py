5867aaa41bb497038cd1651e2ceca780a8cc702a
SJuneja0 <juneja.sahen@gmail.com>
2026-05-01 17:08:33 -0400

    Starting final sim setup, will need to refactor at the end to clean things up

---
+++ final/final_env.py
@@ -0,0 +1,57 @@
+from panda_gym.envs.core import RobotTaskEnv
+from panda_gym.pybullet import PyBullet
+from panda_gym.envs.robots.panda import Panda
+
+from final_task import FinalPickAndPlace
+import numpy as np
+import time
+
+
+class FinalEnv(RobotTaskEnv):
+    """My robot-task environment."""
+
+    def __init__(self, render_mode):
+        self.sim = PyBullet(render_mode=render_mode)
+        self.robot = Panda(self.sim)
+        self.task = FinalPickAndPlace(self.sim)        
+
+        super().__init__(self.robot, self.task)
+
+
+if __name__ == "__main__": 
+    import time
+    from camera import camera
+    from PIL import Image
+
+    env = FinalEnv(render_mode="human")
+
+    cam = camera(pb_client=env.sim.physics_client)
+    rgb, _, _, _ = cam.renderEE(robot_id=env.sim._bodies_idx["panda"])
+    img_path = f"final/cam_test.png"
+    Image.fromarray(rgb).save(img_path)
+    
+    observation, info = env.reset()
+
+    first_pos = observation["observation"][0:2]
+    print("First Pose: ", first_pos)
+    for i in range(500):
+        print("i: ", i)
+        obj_pos = observation["achieved_goal"][0:2]
+        # print(obj_pos)
+        robot_pos = observation["observation"][0:2]
+        print("Robot Pose: ", robot_pos)
+        if not (robot_pos == first_pos).all():
+            print("ROBOT MOVED")
+        
+        action = obj_pos - robot_pos
+        print(action)
+        if np.sum(action) < 0.2:
+            print("REACHED GOAL")
+        time.sleep(0.01)
+        observation, reward, terminated, truncated, info = env.step(np.append(np.append(action, 0), 0))
+        
+
+        # if terminated or truncated:
+        #     observation, info = env.reset()
+    
+    print("---terminated sucsessfully---")
---
+++ final/final_task.py
@@ -0,0 +1,77 @@
+# Goal for this task: Put a object on the table, have the robot pick it up and move it to a new location
+from panda_gym.envs.tasks.pick_and_place import PickAndPlace
+import numpy as np
+
+class FinalPickAndPlace(PickAndPlace):
+    def __init__(self, sim):
+        self.sim = sim
+        self.sim.physics_client.setAdditionalSearchPath("data/object2urdf/examples/ycb_assets/")
+        self.object_size = 0.05 #TODO: Get an estimation for the approx height of the urdf objects
+        super().__init__(sim)
+
+    
+
+    def _create_scene(self) -> None:
+        """Create the scene."""
+        self.sim.create_plane(z_offset=-0.4)
+        self.sim.create_table(length=1.1, width=0.7, height=0.4, x_offset=-0.3)
+
+        self.object_id = self.sim.loadURDF(
+            body_name="object",
+            fileName="006_mustard_bottle.urdf",
+            # fileName="002_master_chef_can.urdf",
+            # fileName="soccerball.urdf",
+            basePosition=[0.5, 0, 0.1],
+            # useFixedBase=False
+        )
+
+        self.sim.create_box(
+            body_name="target",
+            half_extents=np.ones(3) * self.object_size / 2,
+            mass=0.0,
+            ghost=True,
+            position=np.array([0.0, 0.0, 0.05]),
+            rgba_color=np.array([0.1, 0.9, 0.1, 0.3]),
+        )
+
+    # Function is used to control obj position after reset (USE THIS)
+    def _sample_object(self) -> np.ndarray:
+        """Randomize start position of object."""
+        object_position = np.array([-0.5, 0.0, 0.2])
+        # noise = self.np_random.uniform(self.obj_range_low, self.obj_range_high)
+        # object_position += noise
+        print("object position: ", object_position)
+        return object_position
+
+    def disp_pos(self, env):
+        pos, orn = self.sim.physics_client.getBasePositionAndOrientation(env.sim._bodies_idx["object"])
+        vel = self.sim.physics_client.getBaseVelocity(env.sim._bodies_idx["object"])
+        print("actual pos:", pos)
+        print("actual vel: ", vel)
+        self.sim.physics_client.addUserDebugLine([0,0,0], [1,0,0], [1,0,0])
+
+    # def _create_scene(self) -> None:
+    #     """Create the scene."""
+
+    # def get_obs(self) -> np.ndarray:
+    #     pass
+
+    # def get_achieved_goal(self) -> np.ndarray:
+    #     pass
+
+    # def reset(self) -> None:
+    #     pass
+
+    # def _sample_goal(self) -> np.ndarray:
+    #     pass
+
+    # def _sample_object(self) -> np.ndarray:
+    #     pass
+
+    # def is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info: Dict[str, Any] = {}) -> np.ndarray:
+    #     pass
+
+    # def compute_reward(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info: Dict[str, Any] = {}) -> np.ndarray:
+    #     pass
+
+
